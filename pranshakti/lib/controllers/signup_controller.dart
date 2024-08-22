70e2c2d3712dfb6f5704a037f562edcee6b1b9ef
neha331 <nehashedage99@gmail.com>
2024-08-22 15:03:36 +0530

    removed hardcoded strings

--- pranshakti/lib/controllers/signup_controller.dart
+++ pranshakti/lib/controllers/signup_controller.dart
@@ -21,18 +21,4 @@ class SignupController extends GetxController {
 }
 
 
-// void verifyEmail() {
-  //   if (user.value.email.isNotEmpty) {
-  //     // Use the validateEmail method from CommonMethods to check if the email is valid
-  //     bool isValid = CommonMethods.validateEmail(user.value.email);
-  //     if (isValid) {
-  //       // Proceed with OTP sending or other operations
-  //       print('Email is valid');
-  //       Get.snackbar('Success', 'Valid email entered');
-  //     } else {
-  //       Get.snackbar('Invalid Email', 'Please enter a valid email address');
-  //     }
-  //   } else {
-  //     Get.snackbar('Error', 'Email field is empty');
-  //   }
-  // }
+
